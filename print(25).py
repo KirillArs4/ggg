2136bd73fcb4c1d78a3f6dc6729fe2a90dd0bb7e
KirillArs4 <81854074+KirillArs4@users.noreply.github.com>
2021-04-03 12:30:26 +0300

    +++++++

---
+++ print(25).py
@@ -0,0 +1,99 @@
+# set_of = {2, 34, 3}
+# primer = []
+# primer.append = (12)
+# primer.append = (114)
+# primer.append = (125)
+# primer.append = ('512')
+# primer.append = (set_of)
+# primer.append (set.of)
+
+# primer_01 = [2, 4, 6]
+
+# primer.extend(primer_01)
+
+# print(primer)
+
+
+# a = [2, 3, 4]
+# s = ('Введите числа')
+#  primer[3] = 12345654
+# print(primer)
+
+# primer += [32,21]
+# print(primer)
+
+# list_of=[]
+# num = 10
+# for i in range(10)
+#     string = input()
+#     list_of.append(string)
+
+
+
+# print(primer[1: 3])
+
+# del premer[::2]
+
+# print(print)
+
+
+# primer.remove(21)
+# print(primer)
+# primer,insert(2, 1001) 1- место 2 - значение
+# print(primer) 
+
+# prinr(primer.sort())
+
+
+# list_of = []
+# a=0
+# b=0
+# for i in range (int(input())
+#     a.append(input())
+#     b.append(input())
+# for i in range (len(0))
+#     print(a[len(a) - 1 - i], b[len(b) - 1 - i]
+
+# list_01=[]
+# for i in range(4):
+#     suma = input
+#     list_01.append(suma)
+
+# for j in list_01:
+#     print(i, end=',')
+
+
+# s=int(input())
+# a=[]
+# n=[]
+# for i in range(l):
+#     q=input()
+#     a.append(q)
+#     w=input()
+#     for i in a:
+#         if k in n: 
+#             n.append(a)
+#             for i in n:
+#             print(i)
+
+
+# num = []
+# n = int(input())
+# for i in range (n):
+#     num.append(int(input()))
+# for i in range (n - 1):
+#     print(num[i] + num[i+1])
+
+one_list = []
+
+for i in range(int(input())):
+    one_list.append(input())
+    analiz = []
+
+for j in range(int(input())):
+    analiz.append (input())
+
+for s in analiz:
+    if s in one_list:
+        print(s)
+
